846272c7c42152fcc6743667d7c3bf61cd401e0d
Felippe Scheidt <felippescheidt@gmail.com>
2023-11-01 21:32:45 -0300

    refatorar o strategy

--- points_strategy.py
+++ points_strategy.py
@@ -5,13 +5,14 @@ class PointsStrategy(ABC):
     def calculate_points(self):
         pass
 
+# Encapsular a logica de calculo dos pontos
 class EasyPointsStrategy(PointsStrategy):
-    def calculate_points(self):
-        return 1
+    def calculate_points(self, peso=1, acerto=1):
+        return peso*acerto
     
 class MediumPointsStrategy(PointsStrategy):
     def calculate_points(self):
-        return 2
+        return peso*acerto
     
 class HardPointsStrategy(PointsStrategy):
     def calculate_points(self):
